acf7efc5ffa53e50e1e8286135053e171d3c0d94
Aniely Martins <aniellymartins@gmail.com>
2022-10-15 14:09:41 -0300

    configure

--- src/assets/config/env.js
+++ src/assets/config/env.js
@@ -1,5 +1,5 @@
 (function (window) {
     window.__env = window.__env || {};
-    window.__env.production = false;
+    window.__env.production = true;
     window.__env.apiUrl = "https://apiadocao.azurewebsites.net/"; //<-Backend
   }(this));
